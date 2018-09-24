91e1180216078b27b50516c2869d9397db55ff88
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-24 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem9.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Problem9
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string pattern = @"(([A-Z].*[0-9])|([0-9].*[A-Z]))";
+            TextBox1.Text = Regex.IsMatch("HelloWorld", pattern).ToString();
+            TextBox2.Text = Regex.IsMatch("HelloWorld123", pattern).ToString();
+        }
+    }
+}
