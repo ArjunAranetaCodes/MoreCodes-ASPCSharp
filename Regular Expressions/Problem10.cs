ff79a22dcb3023168150711130981ef394a900ec
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-24 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Problem10
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string word = "1000";
+            string pattern = @"(\d)(?=(\d{3})+$)";
+            TextBox1.Text = Regex.Replace(word, pattern, "$1,").ToString();
+        }
+    }
+}
