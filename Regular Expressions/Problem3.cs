d90ef16c100df0bde69bc1ad970a6fbc7e854d4b
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-24 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Problem3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string pattern = "([0-9]{4})-([0-9]{2})-([0-9]{2})";
+            TextBox1.Text = Regex.IsMatch("2018-01-02", pattern).ToString();
+            TextBox2.Text = Regex.IsMatch("01-01-02", pattern).ToString();
+        }
+    }
+}
