118b5f2256a44614978e50a99b54a6ad54f2333a
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-24 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem1.cs
@@ -0,0 +1,21 @@
+﻿//Problem 1: Write a program to test if the first character of the String is uppercase.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Problem1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string pattern = "^[A-Z][a-z0-9_-]{3,19}$";
+            TextBox1.Text = Regex.IsMatch("Hello", pattern).ToString();
+            TextBox2.Text = Regex.IsMatch("world", pattern).ToString();
+        }
+    }
+}
