c2ac3bd53f98ca6c907a3d19187c36f31a450f0b
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-25 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem19.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Problem19
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string word = "Hello World123";
+            string pattern = @"[^0-9]";
+            int count = Regex.Replace(word, pattern, "").Count();
+            TextBox1.Text = count.ToString();
+        }
+    }
+}
