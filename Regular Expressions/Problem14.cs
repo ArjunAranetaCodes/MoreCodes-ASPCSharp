0911d74298062959787c88d4b7801e613a8bd4ac
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-25 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem14.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Problem14
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string pattern = @"^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\." +
+                         @"([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$";
+            TextBox1.Text = Regex.IsMatch("192.168.1.1", pattern).ToString();
+            TextBox2.Text = Regex.IsMatch("1.1.1.1.1", pattern).ToString();
+        }
+    }
+}
