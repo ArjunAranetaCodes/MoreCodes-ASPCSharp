db2ffed3e1d5b800f48837d4c9bf5b947d829ee4
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-25 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem18.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Problem18
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string word = "Hello 'World'";
+            string pattern = @"'([^""]*)'";
+            string extract = Regex.Matches(word, pattern)[0].ToString();
+            TextBox1.Text = extract.ToString();
+        }
+    }
+}
