1ecec709d0f9cd79072a3ba04bc002a33eef3d22
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-25 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem11.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Problem11
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string word = "HelloWorldHelloWorld";
+            string pattern = @"Hello";
+            int count = Regex.Matches(word, pattern).Count;
+            TextBox1.Text = count.ToString();
+        }
+    }
+}
