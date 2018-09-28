5436adb49b68b2fe07588fcef212edd4f6748147
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-28 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem1.cs
@@ -0,0 +1,33 @@
+﻿/*
+Problem 1: Write a program to print the number pattern of ones and zeros using loop.
+11111
+11111
+11111
+11111
+11111
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Problem1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    Label1.Text = Label1.Text + "1";
+                }
+                Label1.Text = Label1.Text + "<br/>";
+            }
+        }
+    }
+}
