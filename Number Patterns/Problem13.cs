e0234cc43328f70a9816aaae8dfb0aa32eed6aab
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-30 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem13.cs
@@ -0,0 +1,47 @@
+﻿/*
+Problem 13: Write a program to print the pattern of asterisks using loop.
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Problem13
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            int row = 5;
+            for (int y = 0; y <= row; y++)
+            {
+                for (int x = 0; x < y; x++)
+                {
+                    Label1.Text = Label1.Text + "*";
+                }
+                Label1.Text = Label1.Text + "<br/>";
+            }
+
+            for (int y = row - 1; y > 0; y--)
+            {
+                for (int x = 0; x < y; x++)
+                {
+                    Label1.Text = Label1.Text + "*";
+                }
+                Label1.Text = Label1.Text + "<br/>";
+            }
+        }
+    }
+}
